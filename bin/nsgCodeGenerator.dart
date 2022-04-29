6a83baf9f5a287e4f9a9a25ea0ce9418a8ea1d2e
pro5 <pro5@SRV-FRZ-WEB>
2022-04-29 22:02:19 +0300

    NsgGeneratorArgs

--- bin/nsgCodeGenerator.dart
+++ bin/nsgCodeGenerator.dart
@@ -1,19 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'nsgGeneratorArgs.dart';
 import 'nsgGenerator.dart';
 
 void main(List<String> args) async {
-  var scPath = '', cSharpPath = '', dartPath = '';
-  var doCSharp = false, doDart = false, forceOverwrite = false;
+  var nsgArgs = NsgGeneratorArgs();
   if (args.isNotEmpty) {
-    scPath = args[0];
+    nsgArgs.serviceConfigPath = args[0];
     if (args.contains('-csharp')) {
-      doCSharp = true;
+      nsgArgs.doCSharp = true;
     } else if (args.contains('-dart')) {
-      doDart = true;
+      nsgArgs.doDart = true;
     } else {
-      doCSharp = doDart = true;
+      nsgArgs.doCSharp = nsgArgs.doDart = true;
     }
     if (args.contains('-force') ||
         args.contains('-overwrite') ||
@@ -21,42 +21,39 @@ void main(List<String> args) async {
       print('Overwrite all files? (y/n/cancel)');
       var yn = stdin.readLineSync(encoding: utf8).toLowerCase();
       if (yn != 'y' && yn != 'n') return;
-      forceOverwrite = yn == 'y';
+      nsgArgs.forceOverwrite = yn == 'y';
     }
     for (var i in args) {
       if (i.startsWith('csharp:')) {
-        cSharpPath = i.substring(i.indexOf(':') + 1).trim();
+        nsgArgs.cSharpPath = i.substring(i.indexOf(':') + 1).trim();
       }
       if (i.startsWith('dart:')) {
-        dartPath = i.substring(i.indexOf(':') + 1).trim();
+        nsgArgs.dartPath = i.substring(i.indexOf(':') + 1).trim();
       }
     }
   } else {
     print('Enter path: ');
-    scPath = stdin.readLineSync(encoding: utf8);
-    doCSharp = doDart = true;
+    nsgArgs.serviceConfigPath = stdin.readLineSync(encoding: utf8);
+    nsgArgs.doCSharp = nsgArgs.doDart = true;
   }
-  startGenerator(scPath, doCSharp, doDart, forceOverwrite,
-      cSharpPath: cSharpPath, dartPath: dartPath);
+  startGenerator(nsgArgs);
 }
 
-void startGenerator(
-    String serviceConfigPath, bool doCSharp, bool doDart, bool forceOverwrite,
-    {String cSharpPath = '', String dartPath = ''}) async {
-  var text = await readFile(serviceConfigPath);
+void startGenerator(NsgGeneratorArgs args) async {
+  var text = await readFile(args.serviceConfigPath);
   var generator = NsgGenerator.fromJson(json.decode(text));
-  generator.doCSharp = doCSharp;
-  generator.doDart = doDart;
-  generator.forceOverwrite = forceOverwrite;
-  if (cSharpPath.isNotEmpty) {
-    generator.cSharpPath = cSharpPath;
+  generator.doCSharp = args.doCSharp;
+  generator.doDart = args.doDart;
+  generator.forceOverwrite = args.forceOverwrite;
+  if (args.cSharpPath.isNotEmpty) {
+    generator.cSharpPath = args.cSharpPath;
   }
-  if (dartPath.isNotEmpty) {
-    generator.dartPath = dartPath;
+  if (args.dartPath.isNotEmpty) {
+    generator.dartPath = args.dartPath;
   }
   print('controllers: ${generator.controllers.length}');
   await generator
-      .writeCode(serviceConfigPath)
+      .writeCode(args.serviceConfigPath)
       .whenComplete(() => print('DONE\n'));
 }
 
---
+++ bin/nsgGeneratorArgs.dart
@@ -0,0 +1,11 @@
+class NsgGeneratorArgs {
+  bool forceOverwrite = false;
+  bool doCSharp = false;
+  bool doDart = false;
+  bool copyCsproj = false;
+  bool copyProgramCs = false;
+  bool copyStartupCs = false;
+  String serviceConfigPath = '';
+  String cSharpPath = '';
+  String dartPath = '';
+}
