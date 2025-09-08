4c5968808facca24478a0ea4bbed323e18ec3e2f
AndyProtech <andy.protech@gmail.com>
2025-09-08 15:04:07 +0300

    1

---
+++ bin/test_main_bonplant.dart
@@ -0,0 +1,43 @@
+import 'nsgCodeGenerator.dart';
+import 'nsgGeneratorArgs.dart';
+
+void main(List<String> args) async {
+  var nsgArgs = NsgGeneratorArgs();
+
+  var project = Project.bon_plant_local_app;
+
+  nsgArgs.serviceConfigPath = project.configPath;
+  nsgArgs.cSharpPath = project.csPath;
+  nsgArgs.doCSharp = false;
+  nsgArgs.dartPath = project.dartPath;
+  nsgArgs.doDart = true;
+
+  nsgArgs.copyCsproj = false;
+  nsgArgs.copyProgramCs = false;
+  nsgArgs.copyStartupCs = false;
+
+  startGenerator(nsgArgs);
+}
+
+class Project {
+  static Project bon_plant_local_app = Project('C:/work/zenkov/bon_plant_local_app/lib/model_config',
+      'C:/Users/pro5/source/repos/scif_app_server', 'C:/work/zenkov/bon_plant_local_app/lib/model');
+  // static Project Scif = Project('C:/Users/pro5/source/repos/scif_app_server/model_config',
+  //     'C:/Users/pro5/source/repos/scif_app_server', 'C:/Users/pro5/source/repos/scif_app/lib/model');
+  // static Project TechControl = Project(
+  //     'C:/Users/pro5/source/repos/TechControl/Server/GeneratorConfig',
+  //     'C:/Users/pro5/source/repos/TechControl/Server',
+  //     'C:/Users/pro5/source/repos/TechControl/tech_control_app/lib/model');
+  // static Project Football = Project(
+  //     'C:/Users/pro5/source/repos/FootballersDiary_Server/FootballersDiary/GeneratorConfig',
+  //     'C:/Users/pro5/source/repos/FootballersDiary_Server/FootballersDiary',
+  //     'C:/Users/pro5/source/repos/footballers_diary_app/lib/model');
+  // static Project Tech2Server =
+  //     Project('D:/NSG/GIT/Tech2Server/json', 'D:/NSG/GIT/Tech2Server/', 'D:/NSG/GIT/tech2_app/lib/model');
+  // static Project Storekeeper = Project('C:/Users/pro5/source/repos/StorekeeperServer/model_config',
+  //     'C:/Users/pro5/source/repos/StorekeeperServer', 'C:/Users/pro5/source/repos/StorekeeperClient/lib/model');
+  String configPath;
+  String csPath;
+  String dartPath;
+  Project(this.configPath, this.csPath, this.dartPath);
+}
